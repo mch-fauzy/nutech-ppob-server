70226f9ccbd4a17b892785a287c84f8530d244cd
mch-fauzy <mucha.fauzy@gmail.com>
2024-12-11 17:43:17 +0700

    feat: add multer memory storage and change respone to throw error

--- middlewares/multer-img-middleware.ts
+++ middlewares/multer-img-middleware.ts
@@ -1,4 +1,4 @@
-import multer, { StorageEngine, FileFilterCallback } from 'multer';
+import multer, { FileFilterCallback } from 'multer';
 import { Request } from 'express';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
@@ -8,32 +8,46 @@ import { Failure } from '../utils/failure';
 import { CONSTANT } from '../utils/constant';
 
 const imageValidation = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
-    if (!CONSTANT.ALLOWED_IMAGE_FORMATS.includes(file.mimetype)) {
+    if (!CONSTANT.MULTER.ALLOWED_IMAGE_FORMATS.includes(file.mimetype)) {
         return cb(Failure.badRequest('Unsupported file format. Only JPEG and PNG are allowed'));
     }
 
     cb(null, true);
 }
 
-// Storage Configuration
-const localStorage: StorageEngine = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, CONFIG.APP.IMAGE_STORAGE_PATH!); // Directory where files are stored
-    },
-    filename: (req, file, cb) => {
-        // Generate a unique filename with the original extension
-        const uniqueFilename = `${uuidv4()}${path.extname(file.originalname)}`;
-        cb(null, uniqueFilename);
-    },
-});
-
-// Multer Configuration
+/**
+ * Multer configuration for local storage
+ */
 const uploadImageToLocal = multer({
-    storage: localStorage,
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, CONFIG.APP.IMAGE_STORAGE_DIRECTORY!); // Directory where files are stored
+        },
+        filename: (req, file, cb) => {
+            // Generate a unique filename with the original extension
+            const uniqueFilename = `${uuidv4()}${path.extname(file.originalname)}`;
+            cb(null, uniqueFilename);
+        },
+    }),
     fileFilter: imageValidation,
     limits: {
-        fileSize: 2 * 1024 * 1024, // 2MB file size limit mb = bytes * bytes
+        fileSize: CONSTANT.MULTER.IMAGE_SIZE_LIMIT
     },
-}).single('image'); // Field named 'image' is expected in the request
+}).single(CONSTANT.MULTER.IMAGE_FIELD_NAME);
 
-export { uploadImageToLocal };
+
+/**
+ * Multer configuration for buffer storage 
+ */
+const uploadImageToCloud = multer({
+    fileFilter: imageValidation,
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: CONSTANT.MULTER.IMAGE_SIZE_LIMIT
+    },
+}).single(CONSTANT.MULTER.IMAGE_FIELD_NAME);
+
+export {
+    uploadImageToLocal,
+    uploadImageToCloud
+};
